f1644a84d92324ce19f39d02ac926ebb42c732f5
Angelica <Angelica137@users.noreply.github.com>
2022-01-11 20:45:36 +0000

    .get_letters() returns list of latters, green

---
+++ scripts/robot_name.py
@@ -0,0 +1,7 @@
+import string
+from typing import AsyncIterable
+
+
+def get_letters():
+    alphabet = list(string.ascii_uppercase)
+    return alphabet
--- tests/test_robot_name.py
+++ tests/test_robot_name.py
@@ -1,5 +1,6 @@
-from scripts import *
+from scripts.robot_name import get_letters
 
 
 def test_get_letters():
-    assert get_letters().len == 676
+    assert get_letters() == ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+                             'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
