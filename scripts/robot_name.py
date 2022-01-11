a35eba3ba61d848d4b248ec946c752c54bc2831d
Angelica <Angelica137@users.noreply.github.com>
2022-01-11 21:00:09 +0000

    .get_letters() has 676 elements, green

--- scripts/robot_name.py
+++ scripts/robot_name.py
@@ -4,4 +4,12 @@ from typing import AsyncIterable
 
 def get_letters():
     alphabet = list(string.ascii_uppercase)
-    return alphabet
+    alphabet_2 = alphabet.copy()
+    robot_letters = []
+    for char in alphabet:
+        for elem in alphabet_2:
+            robot_letters.append(char + elem)
+    return robot_letters
+
+
+print(get_letters())
--- tests/test_robot_name.py
+++ tests/test_robot_name.py
@@ -2,5 +2,4 @@ from scripts.robot_name import get_letters
 
 
 def test_get_letters():
-    assert get_letters() == ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
-                             'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    assert len(get_letters()) == 676
