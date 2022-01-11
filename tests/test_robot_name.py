103e91d86258e65b578b663bd0e8d767d6bd5a43
Angelica <Angelica137@users.noreply.github.com>
2022-01-11 21:07:29 +0000

    get_letters() contains YX, green

--- tests/test_robot_name.py
+++ tests/test_robot_name.py
@@ -1,5 +1,13 @@
-from scripts.robot_name import get_letters
+from scripts.robot_name import *
 
 
 def test_get_letters():
     assert len(get_letters()) == 676
+
+
+def test_get_letters_has_YX():
+    assert "YX" in get_letters()
+
+
+def test_name_pool():
+    assert len(name_pool()) == 676000
