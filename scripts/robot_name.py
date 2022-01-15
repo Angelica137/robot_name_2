c90ba1b894447b336a964c1397e0608d187d5e44
Angelica <Angelica137@users.noreply.github.com>
2022-01-15 19:06:51 +0000

    calculate complexity

--- scripts/robot_name.py
+++ scripts/robot_name.py
@@ -4,21 +4,21 @@ from itertools import product
 
 
 def get_letters():
-    alphabet = list(string.ascii_uppercase)
-    alphabet_2 = alphabet.copy()
+    alphabet = list(string.ascii_uppercase)  # O(n)
+    alphabet_2 = alphabet.copy()  # O(n)
     pairs = product(alphabet, alphabet_2)    # O(n^2)
     robot_letters = (''.join(pair) for pair in pairs)  # O(n)
     return robot_letters
 
 
 def name_pool():
-    numbers = (str(num).zfill(3) for num in range(1000))
-    combinations = product(get_letters(), numbers)
-    names = list(''.join(elem) for elem in combinations)
+    numbers = (str(num).zfill(3) for num in range(1000))  # O(n) + O(n)
+    combinations = product(get_letters(), numbers)  # O(n^2) + O(n^2)
+    names = list(''.join(elem) for elem in combinations)  # O(n^2) + O(n)
     return names
 
 
-n = name_pool()
+n = name_pool()  # O(n^2)
 
 
 class Robot:
@@ -26,8 +26,8 @@ class Robot:
         self.name = self.reset()
 
     def reset(self):
-        if not n:
-            raise RuntimeError("No names available")
-        random.shuffle(n)
-        self.name = n.pop()
+        if not n:  # O(n^2)
+            raise RuntimeError("No names available")  # O(n)
+        random.shuffle(n)  # O(n)
+        self.name = n.pop()  # O(1)
         return self.name
